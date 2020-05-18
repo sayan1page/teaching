91529706a0e37f8c08f3d4bbadd6561c3000547e
Sayan Mukhopadhyay <sayan_jdp@yahoo.co.in>
2020-05-18 14:11:26 +0530

    Add files via upload
    Day 1 text

---
+++ Day1.py
@@ -0,0 +1,78 @@
+# Agenda:
+# 1. Introductions
+# 2. Object Oriented Programming
+# 3. Functional Programming
+# 4. Puzzles
+
+#Sandep - present - not complete - */3
+#Jasvinder - present - complete-*
+#Atul - present - complete - 
+#Chanikya - present - complete - */3
+#Venu - present - complete -
+#babar - present - complete -*/2
+#apoorv - present - complete-*
+#pramod - presnt - complte -* 
+#isaac - absent - 
+# ravi - absent
+
+
+a = 10
+b = 5
+
+#swap value a & b without declaring a third vriable
+
+
+
+ a = [ 1, 2, 3, 4,_, 5. ..100,]
+  a = 1 to 100 with one repeatation
+  -len(a) = n  space complexity = time complexity 
+  
+  n = sum(a)
+  n(n+1)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+employees = [{
+    'name': 'Jane',
+    'salary': 90000,
+    'job_title': 'developer'
+}, {
+    'name': 'Bill',
+    'salary': 50000,
+    'job_title': 'writer'
+}, {
+    'name': 'Kathy',
+    'salary': 120000,
+    'job_title': 'executive'
+}, {
+    'name': 'Anna',
+    'salary': 100000,
+    'job_title': 'developer'
+}, {
+    'name': 'Dennis',
+    'salary': 95000,
+    'job_title': 'developer'
+}, {
+    'name': 'Albert',
+    'salary': 70000,
+    'job_title': 'marketing specialist'
+}]
+
+
+#find the average salary of develper
+
+developers = mean(map( lambda e: e.salary if e.job_title =='developer'))
+
+
+
+
+
+
+
+
