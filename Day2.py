bd473f02c9ccf4d1075c834b76722490ef88d59a
Sayan Mukhopadhyay <sayan_jdp@yahoo.co.in>
2020-05-18 14:13:10 +0530

    Add files via upload
    Day 2 text

---
+++ Day2.py
@@ -0,0 +1,90 @@
+# Agenda:
+# 1. Feedback
+	# A total of 8 feedbacks were received from your learners. 
+	#  The major issue with the low rating is the learners were not able to hear you in the session properly. 
+	#the session's topic was different from the topic's mentioned in the agenda. 
+	# There was no discussion on real world business phenomenon, nothing covered on hypothesis and no discussions on inferential stats and its applications.
+	
+	#sayan_jdp@yahoo.co.in
+	
+	
+	#anudeb - other than assignment
+	#sairam - application oriented
+	#gopi - hypothesis testing
+	#animesh - real life experience 
+	# ananth -
+	# manan - *
+	# ashish - 
+	
+	#keggle competetion - sayan1page/reinforcementlearning/feature
+	
+	
+# 2. Probability
+	# Bayes Theorem
+	# Sentiment Analysis
+
+
+# Probability of A and B will die within a year is p and q. What is the probability that only one of them will alive at the end of the year.
+
+
+# Probabilty that a person will hit a target is 0.3. He shoots 10 times. What is the probability, he hits the target?
+
+# A box contains N coins, m of which are fair and rest are biased. 
+# The probability of getting head of biased coin is 2/3. A coin is drawn from the box at random and tossed once. It is head. 
+ #What is the probaility it is biased.
+
+# F => coin is fair    F` => coin is biased => P(F) = m/N  P(F')= (N-m)/N
+# P( H | F') = 2/3  P(H' | F') = 1/3 ; P(H | F) = P(H' | F) = 1/2
+
+# P (F'| H)= 
+
+# P( F` | H ) * P(H) = P( H | F') * P(F') = P ( H & F')
+# P(F' | H ) =[ (2/3)*(N-m)/ N ] / [ P(H) = P(F) * P( H | F) + P(F')* P(H| F')]
+
+# D = Man is Defulter
+# A = Man age > 50
+# I = Income < 10000
+
+# Training  :  P ( A  | D)
+# Testing : P ( D | A ) 
+           # P( D | I)
+			
+			#P(D) = P( D | A ) * P(D | I) * P( D | F1)
+
+
+
+
+A = [ 1 2 2
+      2 5 4
+	  3 8 6]
+
+det ( A ) = 0 
+
+
+
+# Average accuracy = 58%    Std = 4%    Sample Size = 10
+
+# You are 99% confident weight = 58 +/-  3*4 /sqrt(10) = 53 - 47 
+
+
+
+
+Price of ad = A1* country code+ B1 * ad position 
+
+
+
+
+# First time it shows head and second time tail. What is the probability the coin is fair.
+# Naive Bayes Classifier
+
+
+# 3. Mean, Mode, Medium, Standard Deviation, Covaraince Matrix, Correlation
+	# Normalization of data    ( X - m) / s
+	# Feature Importance
+	# Principle Component Analysis
+	# Dealing with categorical data
+	# House Price Prediction 
+	
+# 4. Hypothesis testing
+       # p-value ...
+	 
